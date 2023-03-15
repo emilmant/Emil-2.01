52303ccacdb148c1133e98191f211b506a57e182
Emil Mantashyan <mantashyan1@mail.ru>
2023-03-15 23:41:34 +0400

    Initial Commit

---
+++ Emil 2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emil 2.01
+//
+//  Created by Emil on 15.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
